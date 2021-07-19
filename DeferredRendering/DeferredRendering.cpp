04b6f681fed9a3dba95f909677033336010f7a1b
Taehwan Kim <49132911+taehwan642@users.noreply.github.com>
2021-07-19 17:16:53 +0900

    Initialize

---
+++ DeferredRendering/DeferredRendering.cpp
@@ -0,0 +1,90 @@
+#include "DXUT.h"
+#include "resource.h"
+
+bool CALLBACK IsD3D9DeviceAcceptable( D3DCAPS9* pCaps, D3DFORMAT AdapterFormat, D3DFORMAT BackBufferFormat,
+                                      bool bWindowed, void* pUserContext )
+{
+    IDirect3D9* pD3D = DXUTGetD3D9Object();
+    if( FAILED( pD3D->CheckDeviceFormat( pCaps->AdapterOrdinal, pCaps->DeviceType,
+                                         AdapterFormat, D3DUSAGE_QUERY_POSTPIXELSHADER_BLENDING,
+                                         D3DRTYPE_TEXTURE, BackBufferFormat ) ) )
+        return false;
+
+    return true;
+}
+
+bool CALLBACK ModifyDeviceSettings( DXUTDeviceSettings* pDeviceSettings, void* pUserContext )
+{
+    return true;
+}
+
+HRESULT CALLBACK OnD3D9CreateDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                     void* pUserContext )
+{
+    return S_OK;
+}
+
+HRESULT CALLBACK OnD3D9ResetDevice( IDirect3DDevice9* pd3dDevice, const D3DSURFACE_DESC* pBackBufferSurfaceDesc,
+                                    void* pUserContext )
+{
+    return S_OK;
+}
+
+void CALLBACK OnFrameMove( double fTime, float fElapsedTime, void* pUserContext )
+{
+}
+
+void CALLBACK OnD3D9FrameRender( IDirect3DDevice9* pd3dDevice, double fTime, float fElapsedTime, void* pUserContext )
+{
+    HRESULT hr;
+
+    V( pd3dDevice->Clear( 0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_ARGB( 0, 45, 50, 170 ), 1.0f, 0 ) );
+
+    if( SUCCEEDED( pd3dDevice->BeginScene() ) )
+    {
+        V( pd3dDevice->EndScene() );
+    }
+}
+
+LRESULT CALLBACK MsgProc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam,
+                          bool* pbNoFurtherProcessing, void* pUserContext )
+{
+    return 0;
+}
+
+void CALLBACK OnD3D9LostDevice( void* pUserContext )
+{
+}
+
+void CALLBACK OnD3D9DestroyDevice( void* pUserContext )
+{
+}
+
+INT WINAPI wWinMain( HINSTANCE, HINSTANCE, LPWSTR, int )
+{
+#if defined(DEBUG) | defined(_DEBUG)
+    _CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+#endif
+
+    DXUTSetCallbackD3D9DeviceAcceptable( IsD3D9DeviceAcceptable );
+    DXUTSetCallbackD3D9DeviceCreated( OnD3D9CreateDevice );
+    DXUTSetCallbackD3D9DeviceReset( OnD3D9ResetDevice );
+    DXUTSetCallbackD3D9FrameRender( OnD3D9FrameRender );
+    DXUTSetCallbackD3D9DeviceLost( OnD3D9LostDevice );
+    DXUTSetCallbackD3D9DeviceDestroyed( OnD3D9DestroyDevice );
+    DXUTSetCallbackDeviceChanging( ModifyDeviceSettings );
+    DXUTSetCallbackMsgProc( MsgProc );
+    DXUTSetCallbackFrameMove( OnFrameMove );
+
+    DXUTInit( true, true );
+    DXUTSetHotkeyHandling( true, true, true );
+    DXUTSetCursorSettings( true, true );
+    DXUTCreateWindow( L"DeferredRendering" );
+    DXUTCreateDevice( true, 640, 480 );
+
+    DXUTMainLoop();
+
+    return DXUTGetExitCode();
+}
+
+
